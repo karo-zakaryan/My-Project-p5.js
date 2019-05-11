d6fd3105f521869f27fabea6155630b986584b11
Karo Zakaryan <karo.zakaryan.y@tumo.org>
2019-05-11 10:32:12 +0400

    Simple chat with socket.io

---
+++ Lesson 4/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const messages = [];
+
+app.use(express.static("."));
+app.get('/', (req, res) => {
+    res.redirect('index.html');
+});
+
+io.on('connection', socket => {    
+    for (const i in messages) {
+        io.sockets.emit("display message", messages[i]);
+    }
+
+    socket.on("send message", data => {
+        messages.push(data);
+        io.sockets.emit("display message", data);
+    });
+});
+
+server.listen(8000, () => {
+    console.log("Server listen to 8000 port");
+});
